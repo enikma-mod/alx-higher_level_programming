69fb79a629c89f2819d2d3eba090b2ae9cc15b76
enikma-mod <sirmodika@gmmail.com>
2022-06-30 21:42:56 +0200

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     """Print hidden module"""
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2} != "__":
-            print("{:s}".format(s))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
