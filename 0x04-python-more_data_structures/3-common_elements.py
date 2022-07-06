def5108022d65c276d870d188669237ca1252025
enikma-mod <sirmodika@gmmail.com>
2022-07-06 20:30:46 +0200

    Task 3 - finding common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a_set = set(set_1)
-    b_set = set(set_2)
-
-    if (a_set & b_set):
-        print(a_set & b_set)
-    else:
-        print("NO common elements")
+    return [num for num in set_1 if num in set_2]
+    #return (set_1 & set_2)
