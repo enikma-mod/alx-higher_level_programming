86a18da1422e933413e5428c88f177f8785d64d2
enikma-mod <sirmodika@gmmail.com>
2022-07-06 21:52:59 +0200

    Task 4 - returns only differences

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,6 +2,3 @@
 def only_diff_elements(set_1, set_2):
     return(set_1.difference(set_2))
     return(set_2.difference(set_1))
-
-    # difference() returns the difference of two or more sets as a new set
-    # return set_1 ^ set_2
