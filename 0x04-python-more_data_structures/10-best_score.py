5cab094ca254eec0e7d295381c529393bd566d58
enikma-mod <sirmodika@gmmail.com>
2022-07-07 14:41:11 +0200

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary else None
     fin_max = max(a_dictionary, key=a_dictionary.get)
     return fin_max
+
+    if a_dictionary else None
