70057df9ece1687acc4e08c00ea2491957cbf7dd
enikma-mod <sirmodika@gmmail.com>
2022-07-18 17:31:20 +0200

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+    except (ValueError, TypeError) as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return False
+    return True
