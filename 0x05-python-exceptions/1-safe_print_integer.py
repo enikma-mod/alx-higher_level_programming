83eec7b11d1abcd7a89aa311c8a42a0dc6ca13cd
enikma-mod <sirmodika@gmmail.com>
2022-07-18 13:08:49 +0200

    Task 1 - fixing bugs and whitespaces

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        #isinstance(value, int)
         print("{:d}".format(value))
     except (ValueError, TypeError):
         return False
