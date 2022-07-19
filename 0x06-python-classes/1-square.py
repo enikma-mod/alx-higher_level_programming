bc515bd09d0b5620b8bc2bdc216c833209aa2671
enikma-mod <sirmodika@gmmail.com>
2022-07-19 13:48:43 +0200

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+"""Defines a Square class"""
+
 class Square:
+    """ Initializes the square class with a size arg"""
     def __init__(self, size)
+
     self.__size = size
 
