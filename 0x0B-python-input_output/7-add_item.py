74b77f8ca311f6da7cdfce48e6cc08fb738dc52b
enikma-mod <sirmodika@gmmail.com>
2022-08-02 16:05:50 +0200

    Task 7- fixing the white spaces

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""save_to_json_file
-"""
-import json
+"""Add item script."""
+import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file, using JSON
-    """
+try:
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
 
-    with open(filename, mode="w", encoding="UTF-8") as saveFile:
-        json.dump(my_obj, saveFile)
+argc = len(sys.argv)
+
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
+
+save_to_json_file(lst, "add_item.json")
