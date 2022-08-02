6c13ad1c632176d94f4ba49ab87e47417b2ff433
enikma-mod <sirmodika@gmmail.com>
2022-08-02 19:52:45 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Add item script."""
-import sys
+'''module for add item'''
 
+
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+arglist = list(sys.argv[1:])
+
 try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
